8cf65a7a7419152496a23d069852e2e440ab3f6b
luk-em <66036269+luk-em@users.noreply.github.com>
2020-05-31 21:47:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".larger").click(function () {
+    $("h1").toggleClass("big");
+})
+$(".smaller").click(function () {
+    $("h1").toggleClass("small");
+})
+$(".border").click(function () {
+    $("h1").toggleClass("border");
+})
